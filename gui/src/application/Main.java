10432d832b652fd3d65b8a0047aebb310337be3e
Doris Tang <huiyao.tang@nyu.edu>
2016-04-26 11:00:23 -0400

    * Added gui java files

---
+++ gui/src/application/Main.java
@@ -0,0 +1,69 @@
+package application;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.stage.WindowEvent;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.layout.AnchorPane;
+
+public class Main extends Application {
+
+	private Scene primaryScene;
+	private Stage primaryStage;
+	private AnchorPane rootLayoutController;
+
+	@Override
+	public void start(Stage primaryStage) {
+		System.out.println("start");
+
+		this.primaryStage = primaryStage;
+		primaryStage.setTitle("DiscoBot");
+		initRootLayout();
+
+		primaryStage.show();
+	}
+
+	private void initRootLayout() {
+		// Load root layout from FXML file.
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("RootLayout.fxml"));
+
+		rootLayoutController = new RootLayoutController();
+		loader.setController(rootLayoutController);
+		loader.setRoot(rootLayoutController);
+
+		try {
+			rootLayoutController = (AnchorPane) loader.load();
+
+			primaryScene = new Scene(rootLayoutController);
+			primaryStage.setScene(primaryScene);
+			primaryStage.setResizable(true);
+			primaryStage.centerOnScreen();
+
+			Parent root = primaryScene.getRoot();
+			for (Node node : root.getChildrenUnmodifiable())
+				node.setStyle("-fx-focus-color: -fx-outer-border; -fx-faint-focus-color: transparent;");
+
+		} catch (IOException e) {
+			System.out.println("could not initialize root layout.");
+			e.printStackTrace();
+		}
+
+		primaryStage.setOnCloseRequest(new EventHandler<WindowEvent>() {
+			@Override
+			public void handle(WindowEvent event) {
+				System.out.println("exiting...");
+			}
+		});
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
---
+++ gui/src/application/RootLayoutController.java
@@ -0,0 +1,17 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.Initializable;
+import javafx.scene.layout.AnchorPane;
+
+public class RootLayoutController extends AnchorPane implements Initializable{
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
